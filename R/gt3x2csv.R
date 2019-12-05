26f93b0cb72c876b3587cee72e0b00efe4e00c35
Danilo de Paula <50498294+danilodpsantos@users.noreply.github.com>
2019-12-05 11:30:47 -0300

    Updating documentation

--- R/gt3x2csv.R
+++ R/gt3x2csv.R
@@ -1,8 +1,8 @@
 #' 
-#' gt3x2csv: A package to convert Actigraph proprietary .gt3x files to csv format.
+#' gt3x2csv: A package to convert Actigraph .gt3x files to csv format.
 #' 
-#' @description: This project was designed with the intent of converting files from the gt3x Actigraph generated files to csv files with the same structure used when exporting/converting raw accelerometry data in the csv format directly from the actilife software.
-#' @section  Foo functions:
+#' @description This project was designed with the intent of converting files from the gt3x Actigraph generated files to csv files with the same structure used when exporting/converting raw accelerometry data in the csv format directly from the actilife software.
+#' @section  Package functions
 #' gt3x_2_csv, gt3x_folder_2_csv, gt3x_2_csv_par
 #'
 #' @docType package
