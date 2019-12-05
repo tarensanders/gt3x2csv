0425af733dc5c25f69bc59fb3ded5c50940757a2
Danilo de Paula <50498294+danilodpsantos@users.noreply.github.com>
2019-12-05 10:32:33 -0300

    Including 2 new functions

---
+++ R/gt3_2_csv_multi.R
@@ -0,0 +1,36 @@
+source("gt3x_2_csv.R")
+
+library(tictoc)
+
+#' gt3x_folder_2_csv
+#' 
+#' Converts al the files inside a folder
+#' 
+#' Lists all the .gt3x files in a given folder and converts it to the actilife RAW data csv file
+#' @param folder_path The folder where the files are located
+
+
+gt3x_folder_2_csv <- function(folder_path) {
+  
+  file_names<- list.files(folder_path,
+                          pattern = ".gt3x",
+                          full.names = TRUE)
+  
+  
+  tic(paste("Finished processing all ", length(file_names), " files"))
+  
+  for (i in 1:length(file_names)) {
+    
+    gt3x_2_csv(file_names[i])
+    
+  }
+  
+  unlink(paste0(folder_path,"/unzip",
+                recursive = TRUE))
+  
+  toc()
+  
+}
+
+
+tempo_seq <- 3734.02
