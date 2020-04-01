85dac024cbc55d2826f62d1493ba3a516652da15
Danilo de Paula <danilodpsantos@gmail.com>
2020-04-01 14:57:42 -0300

    Utils for the package, nominating variables and pipe operator

---
+++ R/utils.R
@@ -0,0 +1,18 @@
+#' @title Pipe operator
+#' @keywords internal
+#' @importFrom dplyr %>%
+NULL
+
+
+#---------------------#
+# Setting global variables
+globalVariables(c("Download.Date",
+                  "Last.Sample.Time",
+                  "Start.Date",
+                  "Stop.Date",
+                  "csv_folder",
+                  "file_id",
+                  "i",
+                  "info_filedf",
+                  "value"))
+#--------------------#
