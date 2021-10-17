e085282b658362e9c01bb931aa38d9a630b1a830
Taren Sanders <taren.sanders@acu.edu.au>
2021-10-17 18:56:43 +1100

    Start rewrite of gt3x_2_csv

--- R/gt3x_2_csv.R
+++ R/gt3x_2_csv.R
@@ -1,3 +1,51 @@
+gt3x_2_csv <- function(gt3x_files,
+                       outdir,
+                       progress = FALSE,
+                       parallel = TRUE,
+                       cores = parallel::detectCores() - 1,
+                       logfile = FALSE,
+                       verbose = FALSE,
+                       recursive = TRUE) {
+  setup_log(logfile, verbose)
+
+  proc_type <- check_file_input(gt3x_files)
+
+  # If a directory, convert to a list of files
+  if (proc_type == "directory") {
+    logger::log_trace("Converting directory to a vector of files")
+    gt3x_files <- list_gt3x_rec(gt3x_files, recursive)
+    num_files <- length(gt3x_files)
+    msg <- "Found {crayon::blue(num_files)} gt3x files"
+
+    if (num_files < 1) {
+      stop(msg)
+    }
+
+    logger::log_info(msg)
+
+  }
+
+  validate_gt3x_files(gt3x_files, proc_type)
+
+  test_vec <- c(file.path(gt3x_files, "EE_left_29.5.2017-05-30.gt3x"),
+                file.path(gt3x_files, "SS_left_19.5.2017-05-22.gt3x"),
+                file.path(gt3x_files, "gt3x_2_csv_log.gt3x"))
+
+  test_dir <- "C:\\Users\\taren\\Downloads\\Test gt3x Files"
+
+  test_dir_files <- list_gt3x_rec(test_dir)
+  read.gt3x::have_log_and_info(test_dir_files)
+
+  valid_files <- sapply(test_vec, read.gt3x::have_log_and_info)
+
+  valid_files[!valid_files]
+
+  # If single file, skip the parallel stuff
+
+  # If multiple files, setup the parallel options
+
+}
+
 save_header <- function(gt3x_file,
                         outfile,
                         verbose = FALSE,
---
+++ R/utils.R
@@ -0,0 +1,87 @@
+setup_log <- function(logfile, verbose) {
+  # Set up logging
+  logger::log_layout(logger::layout_glue_colors)
+  if (verbose) {
+    logger::log_threshold("INFO")
+  } else {
+    logger::log_threshold("FATAL")
+  }
+
+  if (isTRUE(logfile)) {
+    # Logfile requested but location not specified
+    logfile <- file.path(outdir, "gt3x_2_csv_log.log")
+  }
+
+  if (is.character(logfile)) {
+    logger::log_info("Creating a log file at {crayon::blue(logfile)}")
+
+    if (file.exists(logfile)) {
+      logger::log_info("Log file already exists. Will be replaced")
+      unlink(logfile)
+    }
+
+    logger::log_threshold("TRACE", index = 2)
+    logger::log_layout(logger::layout_simple, index = 2)
+    logger::log_appender(logger::appender_file(logfile), index = 2)
+    logger::log_success("Log file created")
+  }
+
+  logger::log_errors()
+
+}
+
+
+check_file_input <- function(gt3x_files) {
+  # Check if single file, vector, or directory
+  logger::log_trace("Checking what format gt3x_files is in")
+  single <- directory <- vec <- FALSE
+
+  if (length(gt3x_files) == 1) {
+    if (file_test("-f", gt3x_files)) {
+      proc_type <- "single"
+      logger::log_trace("gt3x_files is a single file")
+    } else if (file_test("-d", gt3x_files)) {
+      proc_type <- "dir"
+      logger::log_trace("gt3x_files is a directory")
+    } else {
+      err <- "Could not find file or directory {crayon::blue(gt3x_files)}"
+      stop(glue::glue(err))
+    }
+  } else {
+    proc_type <- "vector"
+    logger::log_trace("gt3x_files is a vector")
+  }
+
+  return(proc_type)
+
+}
+
+validate_gt3x_files <- function(gt3x_files, proc_type){
+  logger::log_info("Validating files")
+  # Validate the files
+  if (proc_type=="single") {
+    if (!read.gt3x::is_gt3x(gt3x_files) |
+        !read.gt3x::have_log_and_info(gt3x_files)) {
+      err <- "{crayon::blue(gt3x_files)} is not a valid gt3x file"
+      stop(glue::glue(err))
+    }
+  }
+
+  if (proc_type=="directory" | proc_type=="vector" ) {
+    valid_files_num <- sum(sapply(gt3x_files, read.gt3x::have_log_and_info))
+
+    if (valid_files_num < length(gt3x_files)) {
+      err <- "{crayon::blue(gt3x_files)} is not a valid gt3x file"
+      stop(glue::glue(err))
+    }
+  }
+
+  logger::log_success("Files validated")
+
+}
+
+
+list_gt3x_rec <- function(path, recursive=TRUE){
+  files <- list.files(path = path, full.names = TRUE, recursive = TRUE)
+  return(files[read.gt3x::is_gt3x(files)])
+}
